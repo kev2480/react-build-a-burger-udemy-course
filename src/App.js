e0c2be98a2116ec0bcf8231f33661a45edc9426a
Kevin Borrill <kevin.borrill@3sidedcube.com>
2019-04-30 16:21:36 +0100

    Burger Builder component placeholder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from './componants/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 function App() {
   return (
     <div>
-      <Layout></Layout>
+      <Layout>
+        <BurgerBuilder></BurgerBuilder>
+      </Layout>
     </div>
   );
 }
